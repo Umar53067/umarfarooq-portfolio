1752cd103a5819436b5a14360b7c6735cbc000f9
Umar Farooq <umar53067@gmail.com>
2025-01-23 21:30:42 +0500

    #

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
         <div className='container mx-auto px-8 md-px-16 lg:px-24'>
             <h2 className='text-4xl font-bold text-center mb-12'>About Me</h2>
             <div className='flex flex-col md:flex-row items-center md:space-x-12'>
-                <img src='/hero.jpg' alt="" loading="lazy"
+                <img src='/umarfarooq.jpg' alt="" loading="lazy"
                 className='w-72 h-80 object-cover mb-8 md:mb-0'/>
                 <div className="flex-1">
                 <p>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import {FaEnvelope, FaMapMarkedAlt, FaPhone} from 'react-icons/fa'
 
 function Contact() {
   return (
     <div className='bg-black text-white py-20' id="contact">
     <div className='container mx-auto px-8 md-px-16 lg:px-24'>
-        <h2 className='text-4xl font-bold text-center mb-12'>About Me</h2>
+        <h2 className='text-4xl font-bold text-center mb-12'>Contact With Me</h2>
         <div className='flex flex-col md:flex-row items-center md:space-x-12'>
             <div className="flex-1">
                 <h3 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500
